f565b4583624f993ef536f32eaf9647e41616ab7
ar333k <erikuskinas3@gmail.com>
2024-05-17 19:43:22 +0100

    Added header styling, a gif and changed text color to match the gif

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,20 +1,29 @@
 <script>
 
-    let tabs = [
-        { name: "about", link: "#about" },
-        { name: "projects", link: "#projects" },
-        { name: "blog", link: "#blog" },
-        { name: "contact", link: "#contact" }
+let tabs = [
+        { name: "About", link: "#about" },
+        { name: "Projects", link: "#projects" },
+        { name: "Blog", link: "#blog" },
+        { name: "Contact", link: "#contact" }
     ]
+
 
 </script>
 
-<header class="">
-    <div class="">
+
+<header class="flex items-center justify-center fixed top-0 text-xl z-10 w-full">
+
+    <div class="flex justify-center items-center w-full">
+    <div class="px-8">
+        <img src={"images/haunter.gif"} alt="">
+    </div>
+    <div class="flex items-center justify-center text-center gap-x-20 py-10 px-6 mr-44">
         {#each tabs as tab, index}
             <a href={tab.link}>
                 <p>{tab.name}</p>
             </a>
-        {/each}    
-    </div>        
+        {/each}      
+    </div>
+
 </header>
+
