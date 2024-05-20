10d2cef7de6100031a64747efaaa3508be833180
ar333k <erikuskinas3@gmail.com>
2024-05-20 01:37:59 +0100

    Added styling to main page

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,7 +2,13 @@
     
 </script>
 
-<main class="flex flex-col justify-center items-center">
-    <h1 class="glowingtitle font-VT text-6xl">Erikas Krikstelionis</h1>
-    <p class="glowingtitle font-VT text-4xl">Computer Science Bachelors' Graduate</p>
+<main class="flex flex-col justify-center items-center w-full mt-48">
+    <div class="flex flex-col text-center tracking-widest gap-y-2">
+        <h1 class="glowingtitle font-VT text-7xl">Erikas Krikstelionis</h1>
+        <p class="glowingtitle font-VT text-4xl">Computer Science Bachelors' Graduate</p>
+    </div>
+
+    <div>
+        
+    </div>
 </main>
